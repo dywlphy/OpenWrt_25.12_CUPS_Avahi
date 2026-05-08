ab935ff64b7fc68164a63c0c48bfdb08613e1fce
dywlphy <87897108+dywlphy@users.noreply.github.com>
2026-05-09 01:50:30 +0800

    Update diy-part1-25.sh

--- diy-part1-25.sh
+++ diy-part1-25.sh
@@ -10,11 +10,11 @@ echo "===== 配置 feeds 源 ====="
 > feeds.conf
 
 # 先写官方 feeds（保证基础源正确）
-echo "src-git packages https://github.com/openwrt/packages.git;openwrt-24.10" >> feeds.conf
-echo "src-git luci https://github.com/openwrt/luci.git;openwrt-24.10" >> feeds.conf
+echo "src-git packages https://github.com/openwrt/packages.git;openwrt-25.12" >> feeds.conf
+echo "src-git luci https://github.com/openwrt/luci.git;openwrt-25.12" >> feeds.conf
 
 # 再添加第三方 feeds
-echo "src-git immortalwrt https://github.com/immortalwrt/packages.git;openwrt-24.10" >> feeds.conf
+echo "src-git immortalwrt https://github.com/immortalwrt/packages.git;openwrt-25.12" >> feeds.conf
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages.git" >> feeds.conf
 echo "src-git small https://github.com/kenzok8/small.git" >> feeds.conf
 echo "src-git smpackage https://github.com/kenzok8/small-package" >> feeds.conf
